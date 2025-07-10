b504c15c1e957713ea865d5a04c238592c6d0da1
reh13597 <alexlguo18@gmail.com>
2025-07-10 15:00:17 -0700

    Added skeleton page files and setup routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Scanner from './pages/Scanner';
+import Indecisive from './pages/Indecisive';
+import Contact from './pages/Contact';
+import './App.css';
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route exact path ="/" element={<Home />} />
+        <Route path ="/search" element={<Search />} />
+        <Route path ="/scanner" element={<Scanner />} />
+        <Route path ="/indecisive" element={<Indecisive />} />
+        <Route path ="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Indecisive.jsx
@@ -0,0 +1,5 @@
+export default function Indecisive() {
+    return(
+        <h1>Indecisive Page</h1>
+    );
+}
