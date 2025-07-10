2ba66f31c30d5bfcf128946c3e8a1e11ad3b8b88
reh13597 <alexlguo18@gmail.com>
2025-07-10 14:50:05 -0700

    Modified server.cjs in an attempt to fix Render deployment issue

--- server.cjs
+++ server.cjs
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
