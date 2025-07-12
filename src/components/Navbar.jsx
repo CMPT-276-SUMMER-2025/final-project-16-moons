9bb619e69ce22e0d01aa2b4fd2b005e5bdf92987
lmd-16 <dleeannmarie07@gmail.com>
2025-07-12 09:25:16 -0700

    Added formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,30 +5,30 @@ import logo from '../assets/logo.png';
 function Navbar(){
     return (
     <>
-        <nav className="navbar shadow-md" 
+        <nav className="navbar shadow-md pr-4 pl-4 mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6" 
         style={{ backgroundColor: '#DE6B48' }}>
-           <div className="flex-1 flex items-center">
-            <img src={logo} alt="logo" />
-                <Link to="/" className="text-white">
+           <div className="flex-1 flex items-center gap-2">
+            <img src={logo} alt="logo" className="h-8 w-8" />
+                <Link to="/" className="text-white text-lg">
                 Recipedia
                 </Link>
             </div>
 
             <div className="flex-none">
-                <ul className="menu menu-horizontal text-white-300">
+                <ul className="menu menu-horizontal gap-6 text-sm text-white-300">
                 <Link to="/" className="hover:underline">
                 Home
                 </Link>
-                <Link to="/Search">
+                <Link to="/Search" className="hover:underline">
                 Search
                 </Link>
-                <Link to="/Scanner" >
+                <Link to="/Scanner" className="hover:underline">
                 Scanner
                 </Link>
-                <Link to="/Indecisive">
+                <Link to="/Indecisive" className="hover:underline">
                 Indecisive
                 </Link>
-                <Link to="/Contact">
+                <Link to="/Contact" className="hover:underline">
                 Contact
                 </Link>
                 </ul>
