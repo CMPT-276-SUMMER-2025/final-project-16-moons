368e8a56af323142fb72726b5663a9199c2b5049
reh13597 <alexlguo18@gmail.com>
2025-07-13 15:57:16 -0700

    Removed section 2 component reference from Home Page code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import Section1 from '../components/Section1_Home'
-import Section2 from '../components/Section2_Home'
 
 export default function Home() {
     return(
         <div>
             <Section1 />
-            <Section2 />
         </div>
     );
 }
