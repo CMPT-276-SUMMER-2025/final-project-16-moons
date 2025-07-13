e3ac06ded571dc590194a845541bfec3bba31c5b
reh13597 <alexlguo18@gmail.com>
2025-07-13 15:54:53 -0700

    Fully implemented Home Page Section 1, also added a button on section 1 that scrolls the page down to section 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import Section1 from '../components/Section1_Home'
+import Section2 from '../components/Section2_Home'
 
 export default function Home() {
     return(
-        <Section1 />
+        <div>
+            <Section1 />
+            <Section2 />
+        </div>
     );
 }
