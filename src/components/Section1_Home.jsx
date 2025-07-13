38167f8790df230c342dc4a7c51cf09dcb8bbbec
reh13597 <alexlguo18@gmail.com>
2025-07-12 17:36:42 -0700

    Added proper stlying and sizing for vertical designs

--- src/components/Section1_Home.jsx
+++ src/components/Section1_Home.jsx
@@ -1,15 +1,18 @@
+import VerticalDesign1 from './Designs/Vertical1'
+import VerticalDesign2 from './Designs/Vertical2'
+
 export default function Section1_Home() {
     return(
         <div className="grid grid-cols-3">
             <div className="flex justify-center">
-                <h1>a</h1>
+                <VerticalDesign1 />
             </div>
             <div className="grid grid-rows-2 justify-center">
                 <h1>Home Page Section 1</h1>
                 <p>Blah blah blah blah</p>
             </div>
             <div className="flex justify-center">
-                <h1>a</h1>
+                <VerticalDesign2 />
             </div>
         </div>
     );
