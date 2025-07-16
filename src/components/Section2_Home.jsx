9d4f0cb32ff56993cf0c884176b2e4acd5dde4e3
reh13597 <alexlguo18@gmail.com>
2025-07-15 19:38:56 -0700

    Fixed image/icon source path issue on home page section 2

--- src/components/Section2_Home.jsx
+++ src/components/Section2_Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import WhatWeOfferCards from './DesignCompenets/WhatWeOfferCards'
-import arrow from '../assets/icons/down-arrow.png'
+import search from '../assets/icons/search-interface-symbol.png'
+import scanner from '../assets/icons/Scanner.png'
+import indecisive from '../assets/icons/indecisive.png'
+import apple from '../assets/icons/apple.png'
+import carrot from '../assets/icons/carrot.png'
+import meat from '../assets/icons/meat.png'
+import lines from '../assets/images/linesHorizontal.png'
 
 function Section2_Home() {
   return (
@@ -13,19 +19,19 @@ function Section2_Home() {
         </div>
         <div className='grid sm:grid-cols-3 grid-col-1 gap-10 sm:ml-5 sm:mr-5 h-[300px]'>
         <WhatWeOfferCards
-        image='src\assets\icons\search-interface-symbol.png'
+        image={search}
         title='Recipe Search'
         content= 'Search for recipes by name, main ingredient, area, or category!'
         link="#/search"
         />
         <WhatWeOfferCards
-        image='src\assets\icons\Scanner.png'
+        image={scanner}
         title='Nutrition Scanner'
         content= 'Get the nutrition facts of menus, recipes, or food journals by uploading an image!'
         link="#/scanner"
         />
         <WhatWeOfferCards
-        image='src\assets\icons\indecisive.png'
+        image={indecisive}
         title='Feeling Indecisive?'
         content= 'Randomly generate amazing recipes for breakfast, lunch, and dinner!'
         link="#/indecisive"
@@ -34,12 +40,12 @@ function Section2_Home() {
 
         <div>
           <div className='flex justify-center mt-20 gap-8'>
-            <img src="src\assets\icons\apple.png" alt="apple" />
-            <img src="src\assets\icons\carrot.png" alt="apple" />
-            <img src="src\assets\icons\meat.png" alt="apple" />
+            <img src={apple} alt="apple" />
+            <img src={carrot} alt="apple" />
+            <img src={meat} alt="apple" />
           </div>
           <div className='flex justify-center mt-5'>
-            <img src="src\assets\images\linesHorizontal.png" alt="lines" className='w-175' />
+            <img src={lines} alt="lines" className='w-175' />
           </div>
         </div>
 
