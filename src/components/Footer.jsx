4e89c558fdb2b7f8f54abe0abee957e3626c0ec5
alan-palayoor <alan.palayoor@gmail.com>
2025-07-15 17:47:02 -0700

    added logo to assets, made footer into two sections with logo section on left

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,30 +4,37 @@ import { aboutUsLinks } from "../constants";
 export default function Footer() {
     return(
         <footer className="mt-20 bg-base-100 text-secondary-content py-5">
-            {/* logo column */}
-            
-            
-            {/* Two footer links columns */}
-            <div className="grid lg:grid-cols-2 gap-5">
-                <div>
-                    <h2 className="text-lg text-base-content font-semibold mb-2">Features</h2>
-                    <ul className="space-y-1">
-                        {featuresLinks.map((link, index) => (
-                            <li key={index}>
-                                <a className="text-md text-secondary-content hover:text-base-content underline" href={link.href}>{link.text}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-                    <h2 className="text-lg text-base-content font-semibold mb-2">About Us</h2>
-                    <ul className="space-y-1">
-                        {aboutUsLinks.map((link, index) => (
-                            <li key={index}>
-                                <a className="text-md text-secondary-content hover:text-base-content underline" href={link.href}>{link.text}</a>
-                            </li>
-                        ))}
-                    </ul>
+            <div className="container mx-auto px-80">
+                <div className="grid lg:grid-cols-2 gap-8">
+                    {/* logo column */}
+                    <div className="flex items-center space-x-4">
+                        <img src="src/assets/recipedialogo.png" alt="Recipedia Logo" className="w-12 h-12"/>
+                        <span className="text-3xl text-base-content">Recipedia</span>
+                    </div>
+                    
+                    {/* Two footer links columns */}
+                    <div className="grid lg:grid-cols-2 gap-5">
+                        <div>
+                            <h2 className="text-xl text-base-content mb-2">Features</h2>
+                            <ul className="space-y-1">
+                                {featuresLinks.map((link, index) => (
+                                    <li key={index}>
+                                        <a className="text-md text-secondary-content hover:text-base-content underline" href={link.href}>{link.text}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="text-xl text-base-content mb-2">About Us</h2>
+                            <ul className="space-y-1">
+                                {aboutUsLinks.map((link, index) => (
+                                    <li key={index}>
+                                        <a className="text-md text-secondary-content hover:text-base-content underline" href={link.href}>{link.text}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
