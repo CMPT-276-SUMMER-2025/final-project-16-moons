ccb49e88dd76235f10d7f9613bd3ab3045b75509
lmd-16 <dleeannmarie07@gmail.com>
2025-07-19 12:54:31 -0700

    Add icons to page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import meat from '../assets/icons/meat.png'
+import apple from '../assets/icons/apple.png'
+import carrot from '../assets/icons/carrot.png'
+import lines from '../assets/images/linesHorizontal.png'
+
 
 function SearchCategory(){
     const [searchType, setSearchType] = useState("");
@@ -6,15 +11,15 @@ function SearchCategory(){
 
     const placeholderText = {
         ingredient: "Search by main ingredient...",
-        name: "Search by main name...",
-        area: "Search by main area...",    
-        category: "Search by main category..."
+        name: "Search by name...",
+        area: "Search by area...",    
+        category: "Search by category..."
     };
 
     return(
-        <div className="flex justify-center p-4">
+        <div className="w-1/2 p-10 flex flex-col">
             <div className = "flex flex-col items-start p-10 space-y-6 min-w-max">
-                <div className="flex flex-wrap gap-4">
+                <div className="fflex justify-start space-x-8 p-10 min-w-max">
                     <button 
                         onClick={() => setSearchType("name")}
                         className="bg-[#C0BABA] hover:bg-[#DE6B48] text-white py-3 px-4 rounded-full shadow-md hover:shadow-lg transition-all">
@@ -59,14 +64,26 @@ function SearchCategory(){
 }
 export default function Search() {
     return(
-        <div className="flex flex-col md:flex-row min-h-screen">
-            <div className="w-1/2 p-10 text-left text-xl leading-relaxed">
-            <p className="py-5">Only remember part of the name of a recipe?</p>
-            <p className="py-5">Don’t know what you can make with your ingredients?</p>
-            <p className="py-5">Want to try something different?</p>
-            <p className="py-5">Don’t worry! Search for tons of amazing recipes by name, main ingredient, area, or category!  </p>
+        <div className="flex min-h-screen">
+            <div className="w-1/2 p-10 text-left text-xl leading-relaxed flex flex-col min-h-screen">
+                <p className="py-5">Only remember part of the name of a recipe?</p>
+                <p className="py-5">Don’t know what you can make with your ingredients?</p>
+                <p className="py-5">Want to try something different?</p>
+                <p className="py-5">Don’t worry! Search for tons of amazing recipes by name, main ingredient, area, or category!  </p>
+            
+            <div className="flex justify-center space-x-6 mt-auto ">
+                <img src={meat} alt="meat" className="w-15 h-15 object-cover rounded"/>
+                <img src={carrot} alt="carrot" className="w-15 h-15 object-cover rounded"/>
+                <img src={apple} alt="apple" className="w-15 h-15 object-cover rounded"/>
             </div>
+            <div className="flex justify-center mt-4">
+                <img src={lines} alt="lines" className="w-150 h-auto"/>
+            </div>
+
+            </div>
+
         <SearchCategory/>
+        
         </div>
         
     );
