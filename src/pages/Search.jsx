b2601e717c4ae00ce8996d1ba23285cfe55faa8e
lmd-16 <dleeannmarie07@gmail.com>
2025-07-19 09:56:58 -0700

    Added text to search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,10 @@
 export default function Search() {
     return(
-        <div className="flex items-center justify-center h-200">
-            <h1 className="text-7xl">Search Page</h1>
+        <div className="flex flex-col md:flex-row min-h-screen">
+            <p className="w-1/2 p-10 text-left text-2xl leading-relaxed">Only remember part of the name of a recipe?<br />
+            Don’t know what you can make with your ingredients?<br />
+            Want to try something different?<br />
+            Don’t worry! Search for tons of amazing recipes by name, main ingredient, area, or category!  </p>
         </div>
     );
 }
