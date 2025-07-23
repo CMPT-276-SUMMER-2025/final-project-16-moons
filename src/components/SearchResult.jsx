1458db4a56fc2df3ca0fb4f0d3a1a52464affc14
reh13597 <alexlguo18@gmail.com>
2025-07-23 15:13:17 -0700

    Fully implemented Search Page: added functions to handle recipes that don't have data initially, added error handling, added user tips/info, added a loading popup when fetching data, did some minor styling

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,7 +1,10 @@
-export default function SearchResult({ number, name }) {
+export default function SearchResult({ number, name, image, recipe }) {
     return(
         <div className="bg-base-200 p-6 rounded-xl hover:bg-primary hover:text-white transition duration-300 hover:scale-102">
-            <h1>{number}. {name}</h1>
+            <div className="flex flex-row justify-between">
+                <h1 className="text-xl">{number}. {name}</h1>
+                <img src={image} className="h-30 w-30"></img>
+            </div>
         </div>
     );
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,18 +5,30 @@ import carrot from '../assets/icons/carrot.png'
 import lines from '../assets/images/linesHorizontal.png'
 import SearchResult from '../components/SearchResult'
 
+function SearchHint({ searchType }) {
+    const hints = {
+        ingredient: "E.g., 'Chicken' or 'Onion'",
+        name: "E.g., 'Lamb Pilaf' or 'Chicken Curry'",
+        area: "E.g., 'Canadian' or 'Mexican'",
+        category: "E.g., 'Seafood' or 'Dessert'",
+        initial: "Hmmm... Looks like you haven't chosen a search method yet. Pick one above!"
+    }
 
-function SearchCategory({ onSearch }){
-    const [searchType, setSearchType] = useState("");
-    const [inputText, setInputText] = useState("");
+    return (
+        <div className="bg-base-200 p-6 rounded-xl hover:bg-primary hover:text-white transition duration-300 hover:scale-102">
+            <h1>{hints[searchType] || hints.initial}</h1>
+        </div>
+    )
+}
 
+function SearchCategory({ onSearch, searchType, setSearchType, inputText, setInputText, setError, setRecipes }){
     const placeholderText = {
-        ingredient: "Search by main ingredient...",
-        name: "Search by name...",
-        area: "Search by area...",
-        category: "Search by category...",
-        initial: "Pick an option above to start searching!"
-    };
+        ingredient: "Search for recipes by main ingredient...",
+        name: "Search for recipes by name...",
+        area: "Search for recipes by area...",
+        category: "Search for recipes by category...",
+        initial: "Pick an option above to start searching for recipes!"
+    }
 
     const handleSearch = () => {
         if (inputText.trim() && searchType !== "initial") {
@@ -31,42 +43,37 @@ function SearchCategory({ onSearch }){
     }
 
     useEffect(() => {
-        setSearchType("initial");
+        setSearchType("initial")
     }, [])
 
     return(
         <div className="flex flex-col items-start pt-5 space-y-6">
             <div className="flex justify-start space-x-6">
                 <button
-                    onClick={() => setSearchType("area")}
+                    onClick={() => {setSearchType("area"), setInputText(''), setError(''), setRecipes([])}}
                     className={`${searchType === "area" ? "bg-[#DE6B48]" : "bg-[#C0BABA]"}
-                                hover:bg-[#DE6B48] text-white py-2 px-5 rounded-full transition-all tooltip duration-300 hover:scale-110`}
-                    data-tip="E.g.: 'Canada' or 'Mexico'"
+                                hover:bg-[#DE6B48] text-white py-2 px-5 rounded-full transition-all duration-300 hover:scale-110`}
                 >
                         Area
                 </button>
                 <button
-                    onClick={() => setSearchType("name")}
+                    onClick={() => {setSearchType("name"), setInputText(''), setError(''), setRecipes([])}}
                     className={`${searchType === "name" ? "bg-[#DE6B48]" : "bg-[#C0BABA]"}
-                                hover:bg-[#DE6B48] text-white px-4 rounded-full transition-all tooltip duration-300 hover:scale-110`}
-                    data-tip="E.g.: 'Spaghetti' or 'Pizza'"
+                                hover:bg-[#DE6B48] text-white px-4 rounded-full transition-all duration-300 hover:scale-110`}
                     >
                         Name
                 </button>
                 <button
-                    onClick={() => setSearchType("category")}
+                    onClick={() => {setSearchType("category"), setInputText(''), setError(''), setRecipes([])}}
                     className={`${searchType === "category" ? "bg-[#DE6B48]" : "bg-[#C0BABA]"}
-                                hover:bg-[#DE6B48] text-white px-4 rounded-full transition-all tooltip duration-300 hover:scale-110`}
-                    data-tip="E.g.: 'Seafood' or 'Spicy'"
-
+                                hover:bg-[#DE6B48] text-white px-4 rounded-full transition-all duration-300 hover:scale-110`}
                     >
                         Category
                 </button>
                 <button
-                    onClick={() => setSearchType("ingredient")}
+                    onClick={() => {setSearchType("ingredient"), setInputText(''), setError(''), setRecipes([])}}
                     className={`${searchType === "ingredient" ? "bg-[#DE6B48]" : "bg-[#C0BABA]"}
-                                hover:bg-[#DE6B48] text-white px-4 rounded-full transition-all tooltip duration-300 hover:scale-110`}
-                    data-tip="E.g.: 'Chicken' or 'Onion'"
+                                hover:bg-[#DE6B48] text-white px-4 rounded-full transition-all duration-300 hover:scale-110`}
                     >
                         Main Ingredient
                 </button>
@@ -81,7 +88,7 @@ function SearchCategory({ onSearch }){
                 type = "text"
                 placeholder = {placeholderText[searchType]}
                 value = {inputText}
-                onChange = {(e) => setInputText(e.target.value)}
+                onChange = {((e) => setInputText(e.target.value))}
                 onKeyDown={handleKeyPress}
                 disabled={searchType === "initial"}
                 className = "bg-white shadow-xl h-10 px-10 rounded-full text-sm w-full focus:outline-none focus:ring-2 focus:ring-[#C0BABA]">
@@ -93,43 +100,33 @@ function SearchCategory({ onSearch }){
 }
 
 export default function Search() {
+    const [searchType, setSearchType] = useState('')
+    const [inputText, setInputText] = useState('')
     const [recipes, setRecipes] = useState([])
     const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
-    const getApiUrl = (searchType, query) => {
-        const baseURL = 'https://www.themealdb.com/api/json/v1/1'
+    // Get the endpoint for the topic the user has chosen
+    // i.e., area, name, category, or main ingredient
+    const getEndpoint = (searchType, query) => {
+        const baseUrl = 'https://www.themealdb.com/api/json/v1/1'
         const encodedQuery = encodeURIComponent(query)
 
         switch (searchType) {
             case 'name':
-                return `${baseURL}/search.php?s=${encodedQuery}`
+                return `${baseUrl}/search.php?s=${encodedQuery}`
             case 'ingredient':
-                return `${baseURL}/search.php?i=${encodedQuery}`
+                return `${baseUrl}/filter.php?i=${encodedQuery}`
             case 'area':
-                return `${baseURL}/search.php?a=${encodedQuery}`
+                return `${baseUrl}/filter.php?a=${encodedQuery}`
             case 'category':
-                return `${baseURL}/search.php?c=${encodedQuery}`
+                return `${baseUrl}/filter.php?c=${encodedQuery}`
             default:
                 return null
         }
     }
 
-    const formatRecipes = (data) => {
-        if (!data || !data.meals) {
-            return []
-        }
-
-        return data.meals.map(meal => ({
-            id: meal.idMeal,
-            name: meal.strMeal,
-            category: meal.strCategory,
-            area: meal.strArea,
-            image: meal.strMealThumb,
-            instructions: meal.strInstructions,
-            ingredients: meal.strIngredient1 ? extractIngredients(meal) : null
-        }))
-    }
-
+    // Extract and format the ingredients from the JSON data of one recipe
     const extractIngredients = (meal) => {
         const ingredients = []
 
@@ -148,35 +145,108 @@ export default function Search() {
         return ingredients
     }
 
-    const handleSearch = async (searchType, query) => {
-        setError(null);
+    // Format the recipes to have name, image, instructions, and ingredients
+    const formatRecipes = (data) => {
+        if (!data || !data.meals) {
+            return []
+        }
 
+        return data.meals.map(meal => ({
+            id: meal.idMeal,
+            name: meal.strMeal,
+            image: meal.strMealThumb,
+            instructions: meal.strInstructions,
+            ingredients: extractIngredients(meal)
+        }))
+    }
+
+    // In the case where the user chooses area, category, or main ingredient, this function is ran
+    // Calling the API for those topics only return recipe names, not recipe information like ingredients or instructions
+    // So this function is to get the missing data for the recipes when those certain topics are chosen
+    const getRecipeData = async (recipeName) => {
         try {
-            const apiUrl = getApiUrl(searchType, query);
-
-            if (!apiUrl) {
-                throw new Error('Invalid search type');
-            }
-
-            const response = await fetch(apiUrl);
+            const endpoint = getEndpoint('name', recipeName)
+            const response = await fetch(endpoint)
 
             if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
+                throw new Error(`Error: ${response.status}`)
             }
 
-            const data = await response.json();
-            const formattedRecipes = formatRecipes(data);
+            const data = await response.json()
 
-            setRecipes(formattedRecipes);
+            if (data && data.meals && data.meals.length > 0) {
+                return data.meals[0]
+            }
+
+            return null
+        } catch (error) {
+            console.error('Error:', error.message)
+            setError('Failed to fetch recipes. Please try again.')
+            setRecipes([])
+        }
+    }
+
+    // Fill the recipes array with the missing recipe data if it's like the scenario described above
+    const fillRecipeData = async (recipes) => {
+        const updatedRecipes = []
+
+        for (const recipe of recipes) {
+            if (!recipe.ingredients || !recipe.instructions) {
+                const data = await getRecipeData(recipe.name)
+
+                if (data) {
+                    const updatedRecipe = {
+                        ...recipe,
+                        instructions: data.strInstructions || recipe.instructions,
+                        ingredients: extractIngredients(data),
+                    }
+
+                    updatedRecipes.push(updatedRecipe)
+                } else {
+                    updatedRecipes.push(recipe)
+                }
+            } else {
+                updatedRecipes.push(recipe)
+            }
+        }
+
+        return updatedRecipes
+    }
+
+    // This function is ran when the user enters their search or clicks the search button
+    const handleSearch = async (searchType, query) => {
+        setRecipes([])
+        setError(null)
+        setLoading(true)
+
+        try {
+            const endpoint = getEndpoint(searchType, query)
+
+            const response = await fetch(endpoint)
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`)
+            }
+
+            const data = await response.json()
+            let formattedRecipes = formatRecipes(data)
 
             if (formattedRecipes.length === 0) {
-                setError('No recipes found. Try a different search term.');
+                setError('No recipes found. Try a different search term that is relevant to your chosen topic.')
+                setRecipes([])
             }
 
+            formattedRecipes = await fillRecipeData(formattedRecipes)
+
+            setRecipes(formattedRecipes)
+
+            console.log(formattedRecipes)
         } catch (error) {
-            console.error('Search error:', error.message);
-            setError('Failed to fetch recipes. Please try again.');
-            setRecipes([]);
+            console.error('Error:', error.message)
+            setError('Failed to fetch recipes. Please try again.')
+            setRecipes([])
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -187,7 +257,8 @@ export default function Search() {
                 <p className="py-3">Don’t know what you can make with your ingredients?</p>
                 <p className="py-3">Want to try something different?</p>
                 <p className="py-3">Don’t worry! Search for tons of amazing recipes by area, name, category, or main ingredient.  </p>
-                <p className="py-3">You can get started by just clicking on the search method you want to use, on the right!</p>
+                <p className="py-3">You can get started by simply clicking on the search method you want to use, on the right.</p>
+                <p className="py-3">Once you're done searching, click on a recipe to see its detailed information!</p>
                 <div className="flex justify-center space-x-8 mt-auto pr-50">
                     <img src={meat} alt="meat" className="w-15 h-15 object-cover rounded"/>
                     <img src={carrot} alt="carrot" className="w-15 h-15 object-cover rounded"/>
@@ -197,18 +268,28 @@ export default function Search() {
                     <img src={lines} alt="lines" className="w-130 h-auto"/>
                 </div>
             </div>
-            <div className="flex flex-col w-[45%] gap-6">
-                <SearchCategory onSearch={handleSearch}/>
-                <div className="bg-white p-6 rounded-xl shadow-2xl max-h-full space-y-5 overflow-y-auto">
+            <div className="flex flex-col w-[35%] gap-6">
+                <SearchCategory onSearch={handleSearch} searchType={searchType} setSearchType={setSearchType} inputText={inputText} setInputText={setInputText} setError={setError} setRecipes={setRecipes}/>
+
+                <div className="bg-white p-6 rounded-xl shadow-2xl max-h-full flex-1 space-y-5 overflow-y-auto">
+                    <SearchHint searchType={searchType} />
+
                     {error && (
-                        <div className="text-center text-red-500 py-4">
+                        <div className="bg-base-200 p-6 rounded-xl hover:bg-primary hover:text-white transition duration-300 hover:scale-102">
                             {error}
                         </div>
                     )}
 
                     {recipes.map((recipe, idx) => (
-                        <SearchResult key={recipe.id || idx} number={idx + 1} name={recipe.name} recipe={recipe} />
+                        <SearchResult key={idx} number={idx + 1} name={recipe.name} image={recipe.image} recipe={recipe} />
                     ))}
+
+                    {loading && (
+                        <div className="flex flex-row space-x-5">
+                            <p className="text-2xl">Fetching your recipe data, hang tight!</p>
+                            <span className="loading loading-spinner text-primary loading-lg"></span>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
