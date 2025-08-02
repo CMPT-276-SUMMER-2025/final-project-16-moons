ae055a8a0757c891b6c5555fbc975b9234f80fd8
Ajitsingh Arjunsingh Chauhan <113981559+ajit200315@users.noreply.github.com>
2025-08-01 23:18:08 -0700

    New tests added

--- src/Test/Indecisive.test.jsx
+++ src/Test/Indecisive.test.jsx
@@ -1,4 +1,3 @@
-// src/Test/Scanner.test.jsx
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Indecisive from '../pages/Indecisive'
